a65306ee7e5f9f72a242a40bdacd77ac5d0a16e7
purposeconcepts <39604876+purposeconcepts@users.noreply.github.com>
2026-05-31 12:36:43 -0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hub.purposeconcepts.com',
-  integrations: [sitemap()],
+  output: 'static',
 });
