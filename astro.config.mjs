cc9ca34efb17396970d50eed465c4d76b1554939
purposeconcepts <39604876+purposeconcepts@users.noreply.github.com>
2026-06-01 17:35:17 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hub.purposeconcepts.com',
   output: 'static',
+  integrations: [
+    sitemap({
+      filter: (page) => true
+    })
+  ]
 });
